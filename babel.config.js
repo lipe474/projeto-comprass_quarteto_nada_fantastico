dd35777ad6e9297e1b497e595623c9a54c55b41a
Felipe <lipe.ferreira2002@gmail.com>
2023-10-09 16:08:42 -0300

    config: initial config of project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@dtos': './src/dtos',
+            '@hooks': './src/hooks',
+            '@requests': './src/context',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@utils': './src/utils',
+          }
+        },
+      ],
+    ],
   };
 };
