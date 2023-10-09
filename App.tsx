4812072e89a9a507470776cda242f2e016b4532f
Felipe <lipe.ferreira2002@gmail.com>
2023-10-09 16:17:32 -0300

    config: add fonts to project

--- App.tsx
+++ App.tsx
@@ -1,7 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  OpenSans_400Regular,
+  OpenSans_600SemiBold,
+  OpenSans_700Bold,
+  OpenSans_800ExtraBold
+} from "@expo-google-fonts/open-sans";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    OpenSans_400Regular,
+    OpenSans_600SemiBold,
+    OpenSans_700Bold,
+    OpenSans_800ExtraBold
+  });
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
---
+++ src/theme/index.ts
@@ -0,0 +1,37 @@
+export default {
+  COLORS: {
+    WHITE: "#FFFFFF",
+
+    RED_500: "#FF0024",
+    RED_200: "#EA6275",
+
+    GREEN: "#2AA952",
+
+    ORANGE: "#FF8000",
+
+    YELLOW: "#FFCE00",
+
+    BLACK_900: "#000000",
+    BLACK_800: "#111213",
+
+    GRAY_900: "#565656",
+    GRAY_500: "#9B9B9B",
+    GRAY_100: "#B6B6B6"
+  },
+  FONT_FAMILY: {
+    REGULAR: "OpenSans_400Regular",
+    SEMI_BOLD: "OpenSans_600SemiBold",
+    BOLD: "OpenSans_700Bold",
+    EXTRA_BOLD: "Roboto_700Bold",
+    BLACK: "OpenSans_800ExtraBold"
+  },
+  FONT_SIZE: {
+    XXS: 10,
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    XMD: 18,
+    LG: 24,
+    XL: 32
+  }
+};
