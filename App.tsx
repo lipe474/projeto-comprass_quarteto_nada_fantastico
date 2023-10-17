62ebec5ed247916e0b6a3fa00debe364a102f372
Jamille Monteiro <santosjamille998@gmail.com>
2023-10-17 08:59:40 -0300

    feat: add details card

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {
   OpenSans_400Regular,
   OpenSans_600SemiBold,
   OpenSans_700Bold,
-  OpenSans_800ExtraBold
+  OpenSans_800ExtraBold,
 } from "@expo-google-fonts/open-sans";
 
 import theme from "./src/theme";
@@ -12,20 +12,21 @@ import theme from "./src/theme";
 import { Routes } from "@routes/index";
 import { Loading } from "@components/Loading";
 import { ThemeProvider } from "styled-components/native";
-import './src/utils/translation/i18n'
+import "./src/utils/translation/i18n";
+import Checkout from "@screens/Checkout";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     OpenSans_400Regular,
     OpenSans_600SemiBold,
     OpenSans_700Bold,
-    OpenSans_800ExtraBold
+    OpenSans_800ExtraBold,
   });
 
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle="default" translucent />
-      {fontsLoaded ? <Routes /> : <Loading />}
+      {fontsLoaded ? <Checkout /> : <Loading />}
     </ThemeProvider>
   );
 }
